563b6ddfe7782cc7f7ab88a3ab151fc2b66c4011
Sean Nealon <signalflowsean@gmail.com>
2018-10-23 15:01:01 -0400

    find by id

--- scratch/queries.js
+++ scratch/queries.js
@@ -5,34 +5,40 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .then(() => {
-    const searchTerm = 'lady gaga';
-    let filter = {};
+// mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
 
-    if (searchTerm) {
-      filter.title = { $regex: searchTerm };
-    }
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm };
+//     }
 
-    return Note.find(filter).sort({ updatedAt: 'desc' });
-  })
-  .then(results => {
-    console.log('what\'s up yo');
-    console.log(results);
-  })
-  .then(() => {
-    return mongoose.disconnect(); 
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error(err);
-  });
+//     return Note.find(filter).sort({ updatedAt: 'desc' });
+//   })
+//   .then(results => {
+//     console.log('what\'s up yo');
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect(); 
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
 
-  
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => { 
-    return Note.find(); 
+    const id = '000000000000000000000002'; 
+    return Note.findById(id); 
   })
   .then(results => { 
     console.log('Results: ' + results);
+  })
+  .then(() => { 
+    return mongoose.disconnect(); 
+  })
+  .catch(err => { 
+    console.error(`ERROR RUN!!! ${err}`);
   }); 
