9cd561ae339d95d4d68c09c64b84057dcd2703cd
Sean Nealon <signalflowsean@gmail.com>
2018-10-26 12:19:04 -0400

    endpoints template

---
+++ routes/tags.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express'); 
+
+const Folder = require('../models/folder'); 
+const mongoose = require('mongoose'); 
+
+const router = express.Router(); 
+
+router.get('/', (req, res, next) => { 
+
+}); 
+
+router.get('/:id', (req, res, next) => { 
+
+}); 
+
+router.put('/:id', (req, res, next) => { 
+
+}); 
+
+router.post('/', (req, res, next) => { 
+
+}); 
+
+router.delete('/:id', (req, res, next) => { 
+
+}); 
