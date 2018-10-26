967569c66946c621a51fea70b010d071d8b082f3
Sean Nealon <signalflowsean@gmail.com>
2018-10-26 14:52:25 -0400

    tag validation added

--- routes/tags.js
+++ routes/tags.js
@@ -4,10 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const Tag = require('../models/tag');
+const Note = require('../models/note'); 
+
 const mongoose = require('mongoose');
 
 router.get('/', (req, res, next) => {
   Tag.find()
+    .sort({name : 1})
     .then(result => {
       res.json(result);
     })
@@ -18,12 +21,22 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
+  
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
 
+  
   Tag.findById(id)
     .then(result => {
-      res.json(result);
+      res
+        .status(200)
+        .json(result);
     })
     .catch(err => {
+      res.status(400); 
       return next(err);
     });
 });
@@ -35,11 +48,28 @@ router.put('/:id', (req, res, next) => {
   const newItem = {name}; 
   const newArg = {new :true}; 
 
+  if(!name) { 
+    const err = new Error('Missing `name` in request body'); 
+    err.status = 400; 
+    return next(err); 
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
   Tag.findByIdAndUpdate(id, newItem, newArg)
-    .then(() => {
-      res.status(204).end();
+    .then((result) => {
+      res.status(204).json(result);
     })
     .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The folder name already exists');
+        err.status = 400;
+      }
+      res.status(400); 
       return next(err);
     });
 });
@@ -50,14 +80,25 @@ router.post('/', (req, res, next) => {
 
   const newTag = { name };
 
+  if(!name) { 
+    const err = new Error('Missing `name` in request body'); 
+    err.status = 400; 
+    return next(err); 
+  }
+
   Tag.create(newTag)
     .then(result => {
+      Note.$pull({ tagId : newTag.id}); 
       res
         .location(`/api/tags/${id}`)
-        .status(204)
+        .status(201)
         .json(result);
     })
     .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('The folder name already exists');
+        err.status = 400;
+      }
       return next(err);
     });
 });
